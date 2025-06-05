fcd54713b42849eaeeb3e6d60ae0f232cc23a93f
thpossidente <tposside@gmail.com>
2025-06-05 13:47:30 -0400

    updated group tfce analysis for lefco style gradient analysis

---
+++ scripts/group_tfce.m
@@ -0,0 +1,100 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% The purpose of this script is to perform a group level activity
+%%% analysis on the spacetime localizer data sensory drive and WM contrasts
+%%% for visual and auditory stimulation/tasks.
+%%%
+%%% Tom Possidente - December 2024
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath('/projectnb/somerslab/tom/functions/');
+ccc;
+
+%% load subjIDs
+experiment_name = 'spacetime';
+subjDf = load_subjInfo();
+subjDf_cut = subjDf(~strcmp(subjDf.([experiment_name,'Runs']),''),:);
+%subjCodes = subjDf_cut.subjCode(~ismember(subjDf_cut.subjCode, {'AH', 'SL', 'RR'}));
+subjCodes = subjDf_cut.subjCode(~ismember(subjDf_cut.subjCode, {'AH', 'SL', 'RR', 'PP', 'MM'}));
+N_subjs = length(subjCodes);
+
+fs_num = 163842;
+
+%% Loop through subjs and contrasts and construct mri_concat command
+%contrasts = {'V-A', 'A-P'};
+contrasts = {'f-vP', 'f-aP', 'f-tP','aA-aP', 'vA-vP', 'tA-tP'};
+%contrasts = {'aA-aP', 'vA-vP', 'tA-tP'};
+N_contrasts = length(contrasts);
+subj_path = '/projectnb/somerslab/tom/projects/sensory_networks_FC/data/unpacked_data_nii_fs_localizer/';
+path_suffix = '/localizer/localizer_contrasts_';
+hemis = {'lh', 'rh'};
+
+% already ran this
+% for hh = 1:length(hemis)
+%     hemi = hemis{hh};
+%     for cc = 1:N_contrasts
+%         cmd = 'mri_concat ';
+%         contrast = contrasts{cc};
+%         for ss = 1:N_subjs
+%             subjCode = subjCodes{ss};
+%             path = [subj_path subjCode path_suffix hemi '/' contrast '/ces.nii.gz'];
+%             cmd = [cmd path ' '];
+%         end
+%         cmd = [cmd '--o ' hemi '.ces.localizer_groupavg_' contrast '.nii'];
+%         unix(cmd);
+%     end
+% end
+
+%% Loop through concatenated data and run glm for each contrast and hemisphere
+groupavg_dir = '/projectnb/somerslab/tom/projects/sensory_networks_FC/data/unpacked_data_nii_fs_localizer/grouplevel/';
+
+% % Already ran this
+% for cc = 1:N_contrasts
+%     contrast = contrasts{cc};
+%     for hh = 1:length(hemis)
+%         hemi = hemis{hh};
+%         outdir = ['/projectnb/somerslab/tom/projects/sensory_networks_FC/data/unpacked_data_nii_fs_localizer/grouplevel/' hemi '.ces.localizer_groupavg_' contrast '.glmres'];
+%         unix(['mri_glmfit --y ' groupavg_dir hemi '.ces.localizer_groupavg_' contrast '.nii --surf fsaverage ' hemi ' --osgm --glmdir ' outdir ' --cortex']);
+%     end
+% end
+
+%% Multiple comparison correction (TFCE)
+% FSL's randomise has a TFCE option but does not support surface data, but
+% PALM does, so using that 
+
+addpath(genpath('/projectnb/somerslab/tom/palm-alpha119'));
+res_path = '/projectnb/somerslab/tom/projects/Frontal_Gradients_Boundaries/data/group_contrast_TFCE/';
+fsdir = '/projectnb/somerslab/tom/projects/sensory_networks_FC/data/recons/fsaverage/surf/';
+tfce_iters = '1000';
+
+for cc = 1:N_contrasts
+    contrast = contrasts{cc};
+    for hh = 1:length(hemis)
+        hemi = hemis{hh};
+        input = [res_path hemi '.ces.localizer_groupavg_' contrast '.nii'];
+        outdir = [res_path contrast '/' hemi '/'];
+        if ~isfolder(outdir)
+            mkdir(outdir)
+        end
+        
+        % Reshape input data because FSL doesn't work nicely with freesurfer surface outputs
+        hdr = spm_vol(input);
+        data = spm_read_vols(hdr);
+        nframes = size(data,4);
+        data = reshape(data, [fs_num 1 1 nframes]);
+        new_input = [res_path hemi '.ces.localizer_groupavg_' contrast '_reshaped.nii'];
+        for t = 1:nframes
+            hdr(t).fname = new_input;
+            hdr(t).dim = [fs_num 1 1];
+            spm_write_vol(hdr(t), data(:,:,:,t));
+        end
+        
+        % Run tfce with PALM
+        palm('-i', new_input, '-d', [res_path 'design19.mat'], '-t' ,[res_path 'design.con'], '-s',...
+            [fsdir hemi '.pial'], '-n', tfce_iters, '-ise', '-T', '-tfce2D', '-logp', '-o', outdir)
+
+    end
+end
+
+%% PALM is outputting in nifti-2 format for some reason, so use __.ipynb to convert back into nifti-1 so matlab/freesurfer can use the files
+
+
